47dcd6166279c9b37353549e024dc371aaee3fde
websitedeb <139923826+websitedeb@users.noreply.github.com>
2024-03-02 13:10:49 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -3,6 +3,11 @@ const dnt = document.getElementById("dnt");
 const btn = document.getElementById("btn");
 const box = document.getElementById("box");
 
+const tbxp = document.getElementById("tbx-p");
+const dntp = document.getElementById("dnt-p");
+const btnp = document.getElementById("btn-p");
+const boxp = document.getElementById("box2");
+
 const socket = io();
 
 socket.on("previous messages", (previousMessages) => {
@@ -31,7 +36,10 @@ btn.addEventListener("click", (e) => {
     }
     
     const formattedMessage = `[${displayName}] (${day} ${date}/${month}/${year} ${hour}:${mins}): ${message}`;
+    const user = socket.id;
+    console.log(user)
     socket.emit("chat message", formattedMessage);
+    socket.emit("server user check", displayName, user);
     
     tbx.value = "";
 });
@@ -40,4 +48,38 @@ socket.on("chat message", (msg) => {
     const p = document.createElement("p");
     p.textContent = msg;
     box.appendChild(p);
+});
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+btnp.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const displayName = dnt.value.trim();
+    const userdisplay = dntp.value.trim();
+    const message = tbxp.value.trim();
+    const hour = new Date().getHours();
+    const mins = new Date().getMinutes();
+    const day = new Date().getDay();
+    const date = new Date().getDate();
+    const month = new Date().getMonth();
+    const year = new Date().getFullYear();
+
+    if(!displayName && !message && !userdisplay){
+        alert("Please fill out both the Recivers Display Name and Message fields and Displayname.");
+        return;
+    }    
+
+    const formattedMessage = `[${displayName}] (${day} ${date}/${month}/${year} ${hour}:${mins}): ${message}`;
+    const user = socket.id;
+    socket.emit("private chat message", formattedMessage, userdisplay);
+    socket.emit("server user check", displayName, user);
+    
+    tbxp.value = "";
+});
+
+socket.on("private chat message", (msg) => {
+    const pp = document.createElement("p");
+    pp.textContent = msg;
+    boxp.appendChild(pp);
 });
